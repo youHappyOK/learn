095363285aa6240b3a1508d1679d145b35fd30f4
youHappyOK <luojin_mail@163.com>
2023-07-26 17:56:13 +0800

    1

---
+++ private_ex.py
@@ -0,0 +1,24 @@
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+
+    def get_gender(self):
+        return self.__gender
+
+    def set_gender(self, gender):
+        if gender in {'male', 'female'}:
+            self.__gender = gender
+        else:
+            raise ValueError('wrong gender',gender)
+
+# 测试:
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
