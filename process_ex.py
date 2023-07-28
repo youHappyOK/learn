85c499fa610bcad22d7d96cc6c62eaa138ac1175
youHappyOK <luojin_mail@163.com>
2023-07-28 12:18:50 +0800

    1

--- process_ex.py
+++ process_ex.py
@@ -1,5 +1,6 @@
-from multiprocessing import Process
-import os
+from multiprocessing import Process, Queue
+import os, time, random
+import subprocess
 
 
 # windows无法运行
@@ -21,6 +22,7 @@ def run_proc(name):
     print('Run child process %s (%s)...' % (name, os.getpid()))
 
 # join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步。
+print(__name__, os.getpid())
 if __name__=='__main__':
     print('Parent process %s.' % os.getpid())
     p = Process(target=run_proc, args=('test',))
@@ -28,3 +30,64 @@ if __name__=='__main__':
     p.start()
     p.join()
     print('Child process end.')
+# 子进程会执行与主进程的同一份代码
+print("123", os.getpid())
+
+
+# 进程池
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+#
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     # 对Pool对象调用join()方法会等待所有子进程执行完毕，调用join()之前必须先调用close()，调用close()之后就不能继续添加新的Process了
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# print('$ nslookup www.python.org', os.getpid())
+# r = subprocess.call(['nslookup', 'www.python.org'])
+# print('Exit code:', r)
+
+# print('$ nslookup')
+# p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+# print(output.decode('utf-8'))
+# print('Exit code:', p.returncode)
+
+# 写数据进程执行的代码:
+# def write(q):
+#     print('Process to write: %s' % os.getpid())
+#     for value in ['A', 'B', 'C']:
+#         print('Put %s to queue...' % value)
+#         q.put(value)
+#         time.sleep(random.random())
+
+# 读数据进程执行的代码，进程间通信:
+# def read(q):
+#     print('Process to read: %s' % os.getpid())
+#     while True:
+#         value = q.get(True)
+#         print('Get %s from queue.' % value)
+#
+# if __name__=='__main__':
+#     # 父进程创建Queue，并传给各个子进程：
+#     q = Queue()
+#     pw = Process(target=write, args=(q,))
+#     pr = Process(target=read, args=(q,))
+#     # 启动子进程pw，写入:
+#     pw.start()
+#     # 启动子进程pr，读取:
+#     pr.start()
+#     # 等待pw结束:
+#     pw.join()
+#     # pr进程里是死循环，无法等待其结束，只能强行终止:
+#     pr.terminate()
