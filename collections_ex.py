85c1ae07b9e6c9b3d22a7effbf32ee6e8aaddd59
youHappyOK <luojin_mail@163.com>
2023-07-31 18:09:33 +0800

    1

---
+++ collections_ex.py
@@ -0,0 +1,61 @@
+from collections import namedtuple, deque, defaultdict, OrderedDict
+
+# namedtuple是一个函数，它用来创建一个自定义的tuple对象，并且规定了tuple元素的个数，并可以用属性而不是索引来引用tuple的某个元素。
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(1, 2)
+print('p.x:%s, p.y:%s' % (p.x, p.y))
+
+print(isinstance(p, Point))
+print(isinstance(p, tuple))
+
+Circle = namedtuple('Circle', ['x', 'y', 'r'])
+
+q = deque(['a', 'b', 'c'])
+q.append('x')
+q.appendleft('y')
+print("q:%s" % q)
+
+# 使用dict时，如果引用的Key不存在，就会抛出KeyError。如果希望key不存在时，返回一个默认值，就可以用defaultdict：
+dd = defaultdict(lambda: 'N/A')
+dd['key1'] = 'abc'
+print(dd['key1']) # key1存在
+print(dd['key2']) # key2不存在，返回默认值
+
+
+# 使用dict时，Key是无序的。在对dict做迭代时，我们无法确定Key的顺序。如果要保持Key的顺序，可以用OrderedDict：
+d = dict([('a', 1), ('b', 2), ('c', 3)])
+print(d) # 无序的
+od = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(od)
+# 注意，OrderedDict的Key会按照插入的顺序排列，不是Key本身排序：
+od = OrderedDict()
+od['z'] = 1
+od['y'] = 2
+od['x'] = 3
+print(list(od.keys())) # 按照插入的Key的顺序返回
+
+# FIFO（先进先出）的dict
+class LastUpdatedOrderedDict(OrderedDict):
+
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value):
+        # 三目运算符
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
+
+
+
+
+
+
