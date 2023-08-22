7bc089425b7ed1a7bf8942e910d4090da1438be5
youHappyOK <luojin_mail@163.com>
2023-08-22 17:21:21 +0800

    1

--- base/InjectorTest.py
+++ base/InjectorTest.py
@@ -12,26 +12,14 @@ if __name__ == "__main__":
     injector_instance.binder.bind(Repository, to=Repository("MyRepo"))
 
     # 通过注入器实例创建UserService的实例
+    repo = injector_instance.get(Repository)
     user_service = injector_instance.get(UserService)
     user_service2 = injector_instance.get(UserService2)
 
     # 调用方法，该方法依赖于注入的依赖项
+    repo.sayHello()
     user_service.getUser()
     user_service2.getUser()
-
-    ## 创建一个注入器实例
-    # injector_instance = injector.Injector()
-    #
-    # # 注册依赖项
-    # injector_instance.binder.bind(Database, to=Database("MyDB"))
-    #
-    # # 通过注入器实例创建MyClass的实例
-    # my_class_instance = injector_instance.get(MyClass)
-    # my_class_instance2 = injector_instance.get(MyClass2)
-    #
-    # # 调用方法，该方法依赖于注入的依赖项
-    # my_class_instance.do_something()
-    # my_class_instance2.do_something()
 
 
 
