58b9ba872a5363c64ec09f2e02f80c6f12978690
youHappyOK <luojin_mail@163.com>
2023-08-22 22:55:44 +0800

    1

--- practice/gameProcess/gameProcess.py
+++ practice/gameProcess/gameProcess.py
@@ -1,10 +1,7 @@
-from practice.repository.IniFileRepository import IniFileRepository
-
-
 class GameProcess:
 
     def __init__(self):
         pass
 
     def gameOpration(self):
-        print("游戏运行中")
+        print("process...")
